7bccba5bf67e33acf883ed2077b7b03c85ba1124
bellahOchola <bellahkenya@gmail.com>
2019-09-28 13:04:16 +0300

    add mouseover and mouseout listeners to the images on the portfolio

--- scripts.js
+++ scripts.js
@@ -18,3 +18,75 @@ $(document).ready(function(){
         $(".text1").hide();
     });
 });
+
+$(document).ready(function(){
+    $("#image2").mouseover(function(){
+    $(".text2").show();
+    
+    }).mouseout(function(){
+        $(".text2").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#image3").mouseover(function(){
+    $(".text3").show();
+    
+    }).mouseout(function(){
+        $(".text3").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#image3").mouseover(function(){
+    $(".text3").show();
+    
+    }).mouseout(function(){
+        $(".text3").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#image4").mouseover(function(){
+    $(".text4").show();
+    
+    }).mouseout(function(){
+        $(".text4").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#image5").mouseover(function(){
+    $(".text5").show();
+    
+    }).mouseout(function(){
+        $(".text5").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#image6").mouseover(function(){
+    $(".text6").show();
+    
+    }).mouseout(function(){
+        $(".text6").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#image7").mouseover(function(){
+    $(".text7").show();
+    
+    }).mouseout(function(){
+        $(".text7").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#image8").mouseover(function(){
+    $(".text8").show();
+    
+    }).mouseout(function(){
+        $(".text8").hide();
+    });
+});
