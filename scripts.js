b9bc5fb3a44dc6e2020af6e3ec7e6cbf8e5d1cd3
bellahOchola <bellahkenya@gmail.com>
2019-09-28 10:44:40 +0300

    add mailchimp signup form

--- scripts.js
+++ scripts.js
@@ -1,3 +1,12 @@
 $(".appear").click (function(){
     $(".para").toggle();
 });
+
+$(document).ready(function(){
+    $(".ov1").mouseover(function(){
+    $(".text1").show();
+    
+    }).mouseout(function(){
+        $(".text1").hide();
+    });
+});
