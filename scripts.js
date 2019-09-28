27086aea88d72002c8f3c390b8a65c2ee7c9d058
bellahOchola <bellahkenya@gmail.com>
2019-09-28 12:55:21 +0300

    give the icons a padding

--- scripts.js
+++ scripts.js
@@ -2,6 +2,14 @@ $(".appear").click (function(){
     $(".para").toggle();
 });
 
+$(".dev").click (function(){
+    $(".eng").toggle();
+});
+
+$(".mng").click (function(){
+    $(".iter").toggle();
+});
+
 $(document).ready(function(){
     $("#image1").mouseover(function(){
     $(".text1").show();
